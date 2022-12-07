af431ec74ef2debc2a81ae5d251dd123a5382ca8
reisir <reisirito@gmail.com>
2022-12-08 00:41:50 +0200

    comments and small fixes

--- 07/puzzle.js
+++ 07/puzzle.js
@@ -3,6 +3,7 @@ const lines = require("../getLines")("commands");
 // Answer 1: 1391690
 // Answer 2: 5469168
 
+// The File class only exists so I can call size() on it in Directory.size
 class File {
   _size;
   constructor(size) {
@@ -22,18 +23,18 @@ class Directory {
   }
   size = () =>
     [...this.files, ...Object.values(this.children)].reduce(
-      (total, item) => (total += item.size()),
+      (total, item) => total + item.size(),
       0
     );
 }
 
-// Sizes for part 1
-const notableSize = 100000;
-
+// Initialize "file system"
 const root = new Directory("/");
 let currentDirectory = root;
 
+// Iterate over commands
 for (let i = 0; i < lines.length; i++) {
+  // Get current line and useful substrings
   const line = lines[i];
   const cmd = line.slice(0, 4);
   const rest = line.slice(4).trim();
@@ -41,33 +42,42 @@ for (let i = 0; i < lines.length; i++) {
 
   switch (cmd) {
     case "$ ls": {
+      // noop
       continue;
     }
     case "$ cd": {
       if (last == "/") {
+        // navigate to root
         currentDirectory = root;
         continue;
       }
       if (last == ".") {
+        // navigate to parent
         currentDirectory = currentDirectory.parent;
         continue;
       }
+      // navigate to child
       currentDirectory = currentDirectory.children[rest];
       continue;
     }
     case "dir ": {
+      // check if directory has been listed before
+      // if not, create it
       if (!currentDirectory.children[rest])
         currentDirectory.children[rest] = new Directory(rest, currentDirectory);
       continue;
     }
-    // No command, create a file
     default: {
-      if (!parseInt(line)) continue;
+      // not a command or a directory, must be a file
+      if (!parseInt(line)) continue; // skip lines that don't have a number just in case
       currentDirectory.files.push(new File(parseInt(line)));
       continue;
     }
   }
 }
+
+// Sizes for part 1
+const notableSize = 100000;
 
 // Sizes for part 2
 const totalSpace = 70000000;
@@ -77,17 +87,16 @@ const minSize = neededSpace - (totalSpace - usedSpace);
 
 /**
  * Function that calls transformer on each of a Directory objects children
- * @param {Directory} dir Directory object
+ * @param {Directory} directory Directory object
  * @param {Function} transformer function that accepts two arguments, (size, accumulator)
  * @param {any} accumulator value passed to each transform call
  * @returns {any} `accumulator`
  */
-function reduceChildren(dir, transformer, accumulator = 0) {
-  accumulator = transformer(dir.size(), accumulator);
-  Object.values(dir.children).forEach((child) => {
+function reduceChildren(directory, transformer, accumulator = 0) {
+  Object.values(directory.children).forEach((child) => {
     accumulator = reduceChildren(child, transformer, accumulator);
   });
-  return accumulator;
+  return transformer(directory.size(), accumulator);
 }
 
 // Adds directory size to total if it's under notableSize
