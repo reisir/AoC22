418bec0abb582894d7a5357fde895595cc9a3e55
reisir <reisirito@gmail.com>
2022-12-12 15:16:38 +0200

    day 9 part 2

--- 09/puzzle.js
+++ 09/puzzle.js
@@ -4,33 +4,28 @@ const lines = require("../getLines")("input");
 // Because moveTail was moving tail in between comparisons
 
 // Answer 1: 6209
+// Answer 2: 2460
 
 const uncharted = ".";
 const visited = "#";
 const start = "s";
 
 const world = [[start]];
-
-h = [0, 0];
-t = [0, 0];
-
-const tails = Array(9).fill(t);
+const rope = Array.from({ length: 10 }, () => [0, 0]);
 
 function growWorld(cardinal) {
   const row = Array(world[0].length).fill(uncharted);
   switch (cardinal) {
     case "north":
       world.unshift(row);
-      t[0]++;
-      h[0]++;
+      for (let i = 0; i < rope.length; i++) rope[i][0]++;
       break;
     case "south":
       world.push(row);
       break;
     case "west":
       world.map((r) => r.unshift(uncharted));
-      t[1]++;
-      h[1]++;
+      for (let i = 0; i < rope.length; i++) rope[i][1]++;
       break;
     case "east":
       world.map((r) => r.push(uncharted));
@@ -40,7 +35,32 @@ function growWorld(cardinal) {
   }
 }
 
-function moveTail(head = h, tail = t) {
+function moveHead(direction, head = h) {
+  switch (direction) {
+    case "U":
+      head[0]--;
+      break;
+    case "D":
+      head[0]++;
+      break;
+    case "L":
+      head[1]--;
+      break;
+    case "R":
+      head[1]++;
+      break;
+    default:
+      break;
+  }
+
+  // grow world if head out of bounds
+  if (head[0] === world.length) growWorld("south");
+  if (head[1] === world[0].length) growWorld("east");
+  if (head[0] < 0) growWorld("north");
+  if (head[1] < 0) growWorld("west");
+}
+
+function moveTail(head, tail) {
   const sameCords = (c1, c2) => c1[0] === c2[0] && c1[1] === c2[1];
   const distance = (c1, c2) =>
     Math.max(Math.abs(c2[0] - c1[0]), Math.abs(c2[1] - c1[1]));
@@ -54,41 +74,16 @@ function moveTail(head = h, tail = t) {
   if (head[1] < t1 && distance([t0, t1], head) > 1) tail[1]--;
 }
 
-function markVisited(tail = t) {
+function markVisited(tail) {
   if (world[tail[0]][tail[1]] != start) world[tail[0]][tail[1]] = visited;
 }
 
-function moveHead(direction) {
-  switch (direction) {
-    case "U":
-      h[0]--;
-      break;
-    case "D":
-      h[0]++;
-      break;
-    case "L":
-      h[1]--;
-      break;
-    case "R":
-      h[1]++;
-      break;
-    default:
-      break;
-  }
-
-  // grow world if head out of bounds
-  if (h[0] === world.length) growWorld("south");
-  if (h[1] === world[0].length) growWorld("east");
-  if (h[0] < 0) growWorld("north");
-  if (h[1] < 0) growWorld("west");
-}
-
-function visualize(head = h, tail = t) {
+function visualize(rope) {
   const copy = JSON.parse(JSON.stringify(world));
 
-  //   console.log(h, t);
-  copy[tail[0]][tail[1]] = "T";
-  copy[head[0]][head[1]] = "H";
+  rope.forEach((piece, i) => {
+    copy[piece[0]][piece[1]] = i;
+  });
 
   console.log();
   let s = "";
@@ -102,31 +97,25 @@ function visualize(head = h, tail = t) {
   }
 }
 
+lines.forEach((line) => {
+  let [direction, amount] = line.split(" ");
+  amount = parseInt(amount);
+  console.log("\n", line);
+  for (let i = 0; i < amount; i++) {
+    moveHead(direction, rope[0]);
+    for (let i = 1; i < rope.length; i++) moveTail(rope[i - 1], rope[i]);
+    markVisited(rope.at(-1));
+    // visualize(rope);
+  }
+});
+
+console.log(countVisited());
+
 function countVisited() {
   return world.reduce(
     (total, row) =>
       total +
       row.reduce((total2, location) => total2 + (location === visited), 0),
-    1 // start counts
+    1 // the start counts as visited
   );
 }
-
-// parse directions
-lines.forEach((line) => {
-  let [direction, amount] = line.split(" ");
-
-  amount = parseInt(amount);
-
-  // console.log();
-  // console.log(line);
-  for (let i = 0; i < amount; i++) {
-    moveHead(direction);
-    moveTail();
-    markVisited(t);
-    // visualize();
-  }
-});
-
-// visualize();
-
-console.log(countVisited());
